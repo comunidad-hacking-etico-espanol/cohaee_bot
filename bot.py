a5f1d641b0984576f37e98d7a4cbbea888a4f241
Juan Arriagada <jarrcan@gmail.com>
2019-04-28 10:48:14 -0400

    version inicial

--- bot.py
+++ bot.py
@@ -9,8 +9,6 @@ logger = logging.getLogger()
 
 TOKEN = os.getenv("TOKEN")
 ADMINS = os.getenv("ADMINS")
-print(ADMINS)
-
 
 def run(updater):
     PORT = int(os.environ.get("PORT", "8443"))
@@ -18,10 +16,17 @@ def run(updater):
     updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
     updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, TOKEN))
 
+def logger_console(update, accion):
+    logger.info("{} | {} | {} | {}".format(update.effective_user["id"], update.effective_user["username"], update.effective_user.full_name, accion))
+
+def start_handler(bot, update):
+    logger_console(update, "start")
+    update.message.reply_text("Hola! Soy CoHaEE, el bot de la Comunidad de Hacking Ético - Español");
+
 
 def test_handler(bot, update):
-    logger.info("Usuario id {} esta haciendo una prueba.".format(update.effective_user["id"]))
-    update.message.reply_text("Hola! {}, esta es una prueba.".format(update.effective_user["username"])
+    logger_console(update, "test")
+    update.message.reply_text("{} está haciendo pruebas conmigo...".format(update.effective_user.full_name))
 
 
 if __name__ == '__main__':
@@ -29,5 +34,6 @@ if __name__ == '__main__':
     updater = Updater(TOKEN)
 
     updater.dispatcher.add_handler(CommandHandler("test", test_handler))
+    updater.dispatcher.add_handler(CommandHandler("start", start_handler))
 
     run(updater)
