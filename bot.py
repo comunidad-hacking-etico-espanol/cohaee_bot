6ed9bd2e7dc9ad8401d92e04ef7243c2bedc0114
jarrcan-cl <jarrcan@gmail.com>
2019-08-02 22:58:55 -0400

    se ajusta para funcionar standalone con bdd - fuera de docker

--- bot.py
+++ bot.py
@@ -1,39 +1,40 @@
 import logging
-import os
-import sys
-
 from telegram.ext import Updater, CommandHandler
+
+from settings import *
+# from database import *
+
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger()
 
-TOKEN = os.getenv("TOKEN")
-ADMINS = os.getenv("ADMINS")
 
-def run(updater):
-    PORT = int(os.environ.get("PORT", "8443"))
-    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
-    updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
-    updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, TOKEN))
-
-def logger_console(update, accion):
-    logger.info("{} | {} | {} | {}".format(update.effective_user["id"], update.effective_user["username"], update.effective_user.full_name, accion))
-
-def start_handler(bot, update):
-    logger_console(update, "start")
-    update.message.reply_text("Hola! Soy CoHaEE, el bot de la Comunidad de Hacking Ético - Español");
+def logger_console(user, extra):
+    logger.info("{} | {} | {} | {}".format(user.id, user.username, user.full_name, str(extra) ))
 
 
-def test_handler(bot, update):
-    logger_console(update, "test")
-    update.message.reply_text("{} está haciendo pruebas conmigo...".format(update.effective_user.full_name))
+#def start_handler(update, context):
+#    logger_console(update, "start")
+#    update.message.reply_text("Hola! Soy CoHaEE, el bot de la Comunidad de Hacking Ético - Español");
 
 
-if __name__ == '__main__':
+def command_handler(update, context):
+    if update.effective_user.username in ADMIN_LIST:
+        logger_console(update.effective_user, ["command", "autorizado"])
+        update.message.reply_text("Procesando comando, por favor espere.")
+
+
+def error_handler(update, context):
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+# inicializa el bot
+def bot_start():
     logger.info("Iniciando COHAEE bot")
-    updater = Updater(TOKEN)
+    updater = Updater(TELEGRAM_TOKEN, use_context=True)
 
-    updater.dispatcher.add_handler(CommandHandler("test", test_handler))
-    updater.dispatcher.add_handler(CommandHandler("start", start_handler))
+    updater.dispatcher.add_handler(CommandHandler("command", command_handler))
+    updater.dispatcher.add_error_handler(error_handler)
 
-    run(updater)
+    updater.start_polling()
+    updater.idle()
