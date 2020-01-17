e874a775606c6b64f1702cfee060cc9657ebeb46
jarrcan-cl <jarrcan@gmail.com>
2020-01-17 12:02:48 -0300

    ajuste en modelos

--- models/chat.py
+++ models/chat.py
@@ -7,25 +7,33 @@ class Chat(object):
         self.document = document
         self.id = id
         self.type = type
-
-        if username is not None:
-            self.username = username
-        if title is not None:
-            self.title = title
-        if oficial is not None:
-            self.oficial = oficial
+        self.username = username
+        self.title = title
+        self.oficial = oficial
 
     @staticmethod
     def from_dict(source, document):
-        chat = Chat(id=source['id'], type=source['type'], title=source['title'], document=document)
+        chat = Chat(
+            id=source['id'],
+            type=source['type'],
+            title=source['title'],
+            document=document)
+
         if source['username'] is not None:
             chat.username = source['username']
         if source['title'] is not None:
             chat.title = source['title']
+
         return chat
 
     def to_dict(self):
-        return {"id": self.id, "type": self.type, "title": self.title, "username": self.username, "oficial": self.oficial}
+        return {
+            "id": self.id,
+            "type": self.type,
+            "title": self.title,
+            "username": self.username,
+            "oficial": self.oficial}
 
     def __repr__(self):
-        return 'Chat(document="{}", id="{}", type="{}", title="{}", username="{}", oficial="{}")'.format(self.document, self.id, self.type, self.title, self.username, self.oficial)
+        return 'Chat(document="{}", id="{}", type="{}", title="{}", username="{}", oficial="{}")'\
+                .format(self.document, self.id, self.type, self.title, self.username, self.oficial)
--- models/pregunta.py
+++ models/pregunta.py
@@ -1,17 +1,30 @@
 class Pregunta(object):
-    def __init__(self, pregunta, user, disponible=True, pendiente=False, document=None):
+    def __init__(self, pregunta, user, fecha=None, disponible=True, pendiente=False, document=None):
         self.document = document
         self.pregunta = pregunta
         self.user = user
         self.disponible = disponible
         self.pendiente = pendiente
+        self.fecha = fecha
 
     @staticmethod
     def from_dict(source, document):
-        return Pregunta(pregunta=source['pregunta'], user=source['user'], disponible=source['disponible'], pendiente=source['pendiente'], document=document)
+        return Pregunta(
+            pregunta=source['pregunta'],
+            user=source['user'],
+            fecha=source['fecha'],
+            disponible=source['disponible'],
+            pendiente=source['pendiente'],
+            document=document)
 
     def to_dict(self):
-        return {"pregunta": self.pregunta, "user": self.user, "disponible": self.disponible, "pendiente": self.pendiente}
+        return {
+            "pregunta": self.pregunta,
+            "user": self.user,
+            "fecha": self.fecha,
+            "disponible": self.disponible,
+            "pendiente": self.pendiente}
 
     def __repr__(self):
-        return 'Pregunta(document="{}", pregunta="{}", user="{}", disponible="{}", pendiente="{}")'.format(self.document, self.pregunta, self.user, self.disponible, self.pendiente)
+        return 'Pregunta(document="{}", pregunta="{}", user="{}", fecha="{}", disponible="{}", pendiente="{}")'\
+                .format(self.document, self.pregunta, self.user, self.fecha, self.disponible, self.pendiente)
